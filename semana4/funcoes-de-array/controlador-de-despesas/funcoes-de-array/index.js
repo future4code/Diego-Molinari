e7e84891c69929988c692fd084abe59a01cb845b
diegotjmolinari <diego.tj.molinari@gmail.com>
2020-05-07 20:58:21 -0300

    exercicio bebidas

--- semana4/funcoes-de-array/controlador-de-despesas/funcoes-de-array/index.js
+++ semana4/funcoes-de-array/controlador-de-despesas/funcoes-de-array/index.js
@@ -1,23 +1,71 @@
+let guardaDespesa = []
 
-
-
-/* function cadastrarDespesas(evento){
-    
-    let despesa = {
-        valor: document.getElementById('valor').value,
-        tipo: document.getElementById('tipo').value,
-        descricao: document.getElementById('descricao').value,
-    }
+ function cadastrarDespesas(evento){
 
     const meuValor = document.getElementById('valor');
-    console.log(meuValor.value)
+    const meuTipo = document.getElementById('tipo');
     const minhaDescricao = document.getElementById('descricao');
-    console.log(minhaDescricao.value) 
 
-    document.getElementById("valor").value = ""
-    document.getElementById("descricao").value = ""
+    if(meuValor.value === "" || meuTipo.value === "" || minhaDescricao.value === "" )
+    {
+        alert ("insira todos os valores")
+    }
+        else 
+        { 
+
+            let despesa = {
+                valor: meuValor.value,
+                tipo: meuTipo.value,
+                descricao: minhaDescricao.value
+            }
+        
+            guardaDespesa.push(despesa)
+            console.log(guardaDespesa)
+        
+            document.getElementById("valor").value = ""
+            document.getElementById("descricao").value = ""
+        }
+    }
+
+    function filtraValor(){
+
+
+    const filtroValorMaximo = document.getElementById('valorMaximo');
+    const filtroValorMinimo = document.getElementById('Valorminimo');
+    const filtroValorTipo = document.getElementById('categoria');
+    const mostraDespesa = document.getElementById('mostraDespesa');
+
+    if(filtroValorTipo.value === "cerveja")
+    {
+        guardaDespesa.filter (despesa =>{
+            if(despesa.tipo === "cerveja"){
+                console.log(despesa)
+                return despesa;
+            }
+            
+
+        })
+    } else if(filtroValorTipo.value === "vodka")
+    {
+        guardaDespesa.filter (despesa =>{
+            if(despesa.tipo === "vodka"){
+                console.log(despesa)
+                mostraDespesa.innerHTML += `<ul> <li> ${despesa.tipo}</li> <li>${despesa.descricao}</li> <li>${despesa.valor}</li></ul>`
+                
+
+            }
+        } )   
+    }
 }
- */
+
+
+
+
+    
+
+
+ 
+
 
 
 
