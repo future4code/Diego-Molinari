47c46b13bbd96155fa6d140433b4bd5f08882bb0
diegotjmolinari <diego.tj.molinari@gmail.com>
2020-05-07 17:05:15 -0300

    Exercicos nao resolvidos

---
+++ semana4/funcoes-de-array/controlador-de-despesas/funcoes-de-array/index.js
@@ -0,0 +1,56 @@
+
+
+
+/* function cadastrarDespesas(evento){
+    
+    let despesa = {
+        valor: document.getElementById('valor').value,
+        tipo: document.getElementById('tipo').value,
+        descricao: document.getElementById('descricao').value,
+    }
+
+    const meuValor = document.getElementById('valor');
+    console.log(meuValor.value)
+    const minhaDescricao = document.getElementById('descricao');
+    console.log(minhaDescricao.value) 
+
+    document.getElementById("valor").value = ""
+    document.getElementById("descricao").value = ""
+}
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
