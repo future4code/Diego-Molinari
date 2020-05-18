287b33af7d8103e81d42b5e6797acc5ed2d2559d
diegotjmolinari <diego.tj.molinari@gmail.com>
2020-05-18 16:30:38 -0300

    exercicios sem o desafio

---
+++ semana6/exercicio-formlab/formlab/src/components/Etapa3.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//import './App.css';
+
+function Etapa3() {
+  return (
+    <div className="Etapa3">
+      <h1>ETAPA 3 - INFORMAÇÕES GERAIS DE ENSINO</h1>
+      <p>5. Por que você não terminou um curso de graduação?</p>
+      <input />
+      <p>6. Você fez algum curso complementar?</p>
+      <select>
+          <option value="">Não</option>
+          <option value="">Curso de inglês</option>
+          <option value="">Curso técnico</option>
+      </select>
+    </div>
+  );
+}
+
+export default Etapa3;
