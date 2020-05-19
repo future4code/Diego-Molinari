de4bc3a8a467a54680913abf0b7e3294cd07ea92
diegotjmolinari <diego.tj.molinari@gmail.com>
2020-05-19 16:41:27 -0300

    exercicios incompletos, continuarei hoje a noite

---
+++ semana6/boiler-plate/build/precache-manifest.c8ad1b96b3d21ead0765f2567b30b32d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f0ee61cd4bbc81fd1d7f4e40cdb53f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33cdd51be361ef4fd918",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "0bc709225786e34ebc3e",
+    "url": "/static/js/2.ce6ba238.chunk.js"
+  },
+  {
+    "revision": "33cdd51be361ef4fd918",
+    "url": "/static/js/main.357f65da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
